685f2b3c53cc099705bd27037738c1cdada89f55
anhtruong <phanhpompap@gmail.com>
2023-10-06 17:00:03 +0200

    front-end

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      Hello
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
