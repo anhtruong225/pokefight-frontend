a8ccd890d09f59e2fafc2bfff98e11f3dc1e4144
Soydan Yuece <soydan.yuece@gmx.de>
2023-10-12 16:44:26 +0200

    Add get 2 random pokemon functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
-import GamePage from "./pages/GamePage";
+import HomePage from "./pages/HomePage.jsx";
+import GamePage from "./pages/GamePage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       Hello
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function PokemonDetails() {
+const PokemonDetails = () => {
+  const baseUrl = "https://pokefight-backend-oyak.onrender.com";
+
+  const [pokemon, setPokemon] = useState([]);
+  const [randomPokemon, setRandomPokemon] = useState([]);
+
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+
+  const getAllPokemon = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/pokemon`);
+      if (response.data.length) {
+        setPokemon(response.data);
+
+        // Ensure the two random Pokemon are different
+        const randomIndices = getRandomIndices(response.data.length, 2);
+        const randomPokemonList = randomIndices.map(
+          (index) => response.data[index]
+        );
+        setRandomPokemon(randomPokemonList);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Helper function to generate random indices
+  const getRandomIndices = (max, count) => {
+    if (max < count) {
+      throw new Error(
+        "Cannot get more random indices than the length of the array."
+      );
+    }
+    const indices = [];
+    while (indices.length < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      if (!indices.includes(randomIndex)) {
+        indices.push(randomIndex);
+      }
+    }
+    return indices;
+  };
+
+  const handleNewPokemon = () => {
+    const randomIndices = getRandomIndices(pokemon.length, 2);
+    const randomPokemonList = randomIndices.map((index) => pokemon[index]);
+    console.log(randomPokemonList);
+    setRandomPokemon(randomPokemonList);
+  };
+
   return (
-    <div>PokemonDetails</div>
-  )
-}
+    <div>
+      PokemonDetails
+      <button
+        onClick={() => {
+          console.log(pokemon);
+        }}
+      >
+        Test Pokemon Data
+      </button>
+      <button onClick={handleNewPokemon}>Generate two new Pokemon</button>
+    </div>
+  );
+};
 
-export default PokemonDetails
+export default PokemonDetails;
