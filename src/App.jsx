5a25c2db5faef8551e0082e8c57aa0bee8a708c0
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-12 16:04:18 +0200

    pokemon card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
-import GamePage from "./pages/GamePage";
+// import React from "react";
+// import HomePage from "./pages/HomePage";
+// import GamePage from "./pages/GamePage";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
-      Hello
-      <HomePage />
+      {/* Hello */}
+      {/* <HomePage /> */}
+      <PokemonCard />
     </div>
   );
 }
