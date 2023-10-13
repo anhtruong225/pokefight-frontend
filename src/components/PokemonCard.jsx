1f1a47165b7b33ad8d3f9de4f46b6cd1a7589795
Soydan Yuece <soydan.yuece@gmx.de>
2023-10-13 20:10:53 +0200

    MVP Game now seems to work

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,79 +2,90 @@ import "./PokemonCard.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function PokemonCard({ pokemonData }) {
-  const name = "Pikachu";
-  const imageUrl =
-    "https://i.postimg.cc/qRd6QFxg/385094558-18393011818009106-4317171623600290976-n.jpg";
-  const type = "Electric";
-  const stats = {
-    HP: 35,
-    Defense: 30,
-    "Sp. Atk": 50,
-    "Sp. Def": 40,
-    Speed: 90,
-  };
-  const level = "50";
-
-  const [pokemonImg, setpokemonImg] = useState("");
+function PokemonCard({
+  pokemonData,
+  showCard,
+  skip,
+  showStats,
+  onClick,
+  winner,
+  chosen,
+}) {
+  const [pokemonImg, setPokemonImg] = useState("");
+  const [isWinner, setIsWinner] = useState(null);
 
   useEffect(() => {
     const getPokemonImg = async () => {
-      const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${pokemonData.name.english.toLowerCase()}`
-      );
-      if (response.data) {
-        setpokemonImg(response.data.sprites.front_default);
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonData.name
+            .toLowerCase()
+            .replace(/\s+/g, "-")}`
+        );
+        if (response.data) {
+          setPokemonImg(response.data.sprites.front_default);
+        }
+      } catch (error) {
+        console.log(error);
+        skip();
       }
     };
+
+    const determineIfWinner = () => {
+      if (winner !== null && chosen !== null) {
+        setIsWinner(chosen === pokemonData.id ? winner === chosen : null);
+      } else {
+        setIsWinner(null);
+      }
+    };
+
     getPokemonImg();
-  }, [pokemonData]);
+    determineIfWinner();
+  }, [pokemonData, winner, chosen, skip]);
+
+  const handleOnClick = () => {
+    onClick(pokemonData.id);
+  };
 
   return (
-    <div className="pokemon-card">
-      <div className="pokemon-details">
-        <div className="pokemon-name">
-          {pokemonData.name.english}
-          <div className={`pokemon-type ${type.toLowerCase()}`}>
-            {pokemonData.type[0]}
+    <div
+      className={`pokemon-card ${
+        showCard && winner === null && "pokemon-card--hover"
+      }  ${showCard && isWinner && "pokemon-card--winner"} ${
+        showCard && isWinner === false && "pokemon-card--loser"
+      }`}
+      onClick={handleOnClick}
+    >
+      {showCard && (
+        <>
+          <div className="pokemon-details">
+            <div className="pokemon-name">
+              {pokemonData.name}
+              <div className={`pokemon-type`}>{pokemonData.type}</div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="pokemon-image-container">
-        <img
-          src={pokemonImg}
-          alt={pokemonData.name.english}
-          className="pokemon-image"
-          width={300}
-          height={300}
-        />
-      </div>
-
-      <div className="pokemon-details">
-        <div className="pokemon-level">Lv. {level}</div>
-        <div className="pokemon-stats">
-          <div className="pokemon-stat">
-            <div className="stat-label">HP</div>
-            <div className="stat-value">{pokemonData.base.HP}</div>
+          <div className="pokemon-image-container">
+            <img
+              src={pokemonImg}
+              alt={pokemonData.name}
+              className="pokemon-image"
+              width={300}
+              height={300}
+            />
           </div>
-          <div className="pokemon-stat">
-            <div className="stat-label">Defense</div>
-            <div className="stat-value">{pokemonData.base.Defense}</div>
+          <div className="pokemon-details">
+            {/* <div className="pokemon-level">Lv. {level}</div> */}
+            <div className="pokemon-power">
+              <div className="stat-label">Power</div>
+              {showStats ? (
+                <div className="stat-value">{pokemonData.power}</div>
+              ) : (
+                <div className="stat-hidden">Hidden</div>
+              )}
+            </div>
           </div>
-          <div className="pokemon-stat">
-            <div className="stat-label">Sp. Atk</div>
-            <div className="stat-value">{pokemonData.base["Sp. Attack"]}</div>
-          </div>
-          <div className="pokemon-stat">
-            <div className="stat-label">Sp. Def</div>
-            <div className="stat-value">{pokemonData.base["Sp. Defense"]}</div>
-          </div>
-          <div className="pokemon-stat">
-            <div className="stat-label">Speed</div>
-            <div className="stat-value">{pokemonData.base.Speed}</div>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
